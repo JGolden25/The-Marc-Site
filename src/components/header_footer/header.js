8ca24fab05c1bca89636042164a0349a55e5de3f
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-13 16:49:35 -0600

    add menu

--- src/components/header_footer/header.js
+++ src/components/header_footer/header.js
@@ -9,8 +9,30 @@ class Header extends Component {
         return (
             <AppBar 
             position="fixed"
+            style={{
+                backgroundColor:'#2f2f2f',
+                boxShadow:'none',
+                padding: '10px 0px'
+            }}
             >
-                header test
+                <Toolbar>
+
+                    <div className="header_logo">
+            <div
+                className="font_righteous header_logo_venue"> The Marc</div>
+            <div className="header_logo_title">
+                Musical Events</div>
+                    </div>
+                    <IconButton
+                    aria-label="Menu"
+                    color="inherit"
+                    onClick={()=> console.log('open')}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+
+                </Toolbar>
+                
             </AppBar>
         );
     }
