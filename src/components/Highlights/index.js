9b9087da8ffc25cadfe94c62fd2287cf5a3e2628
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-18 13:19:15 -0600

    info section complete

---
+++ src/components/Highlights/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Highlights = () => {
+    return (
+        <div className="highlight_wrapper">
+            
+        </div>
+    );
+};
+
+export default Highlights;
--- src/components/venueNfo/index.js
+++ src/components/venueNfo/index.js
@@ -1,65 +1,64 @@
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
+
 import icon_calendar from '../../resources/images/icons/calendar.png';
 import icon_location from '../../resources/images/icons/location.png';
 
-const VenueNfo = () => {
+const VunueNfo = () => {
     return (
-        <div className='bck_black'>
+        <div className="bck_black">
             <div className="center_wrapper">
                 <div className="vn_wrapper">
+
                     <Zoom duration={500}>
-                    <div className="vn_item">
-                    <div classname="vn_outer">
-                    <div classname="vn_inner">
-                        <div classname="vn_icon_square bck_red">
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_red"></div>
+                                    <div 
+                                        className="vn_icon"
+                                        style={{
+                                            background:`url(${icon_calendar})`
+                                        }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Date & Time
+                                    </div>
+                                    <div className="vn_desc">
+                                        5 Janurary 2020 @10:00 pm
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                        </div>
-                        <div
-                        classname="vn_icon"
-                        style={{
-                            background:`url(${icon_calendar})`
-                        }}
-                        ></div>
-                        <div className='vn_title'>
-                            Event Date & Time
-                        </div>
-                        <div className="vn_desc">
-                            5 Jan 2020 @ 10:00 pm
-                        </div>
-                    </div>
-                    </div>
-                </div>
                     </Zoom>
-
+                    
                     <Zoom duration={500} delay={500}>
-                    <div className="vn_item">
-                    <div classname="vn_outer">
-                    <div classname="vn_inner">
-                        <div classname="vn_icon_square bck_yellow">
-
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_yellow"></div>
+                                    <div 
+                                        className="vn_icon"
+                                        style={{
+                                            background:`url(${icon_location})`
+                                        }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Location
+                                    </div>
+                                    <div className="vn_desc">
+                                        2657 W Avondale St San Marcos, Tx.
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div
-                        classname="vn_icon"
-                        style={{
-                            background:`url(${icon_location})`
-                        }}
-                        ></div>
-                        <div className='vn_title'>
-                            Event Location
-                        </div>
-                        <div className="vn_desc">
-                            3521 W. Avondale San Marcos, Tx 78666
-                        </div>
-                    </div>
-                    </div>
-                </div>
                     </Zoom>
-                
+                    
                 </div>
             </div>
         </div>
     );
 };
 
-export default VenueNfo;
+export default VunueNfo;
