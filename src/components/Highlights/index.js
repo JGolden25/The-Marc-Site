f363d5d9360f9d451ce1ff0c3edb73d95ad6ae4d
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-18 13:28:17 -0600

    start highlights

--- src/components/Highlights/index.js
+++ src/components/Highlights/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Description from './Description';
 const Highlights = () => {
     return (
         <div className="highlight_wrapper">
-            
+            <Description/>
         </div>
     );
 };
