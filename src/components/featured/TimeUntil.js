d0bd7b972e78f3eca57c560ed03ff6c8d43340e6
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-18 02:19:24 -0600

    days unit measure fixed

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -20,7 +20,7 @@ class TimeUntil extends Component {
             const seconds = Math.floor((time/1000)%60);
             const minutes = Math.floor((time/1000/60)%60);
             const hours = Math.floor((time/1000*60*60)%24);
-            const hours = Math.floor((time/1000*60*60*24));
+            const days = Math.floor(time/(1000*60*60*24));
 
             this.setState({
                 days,
