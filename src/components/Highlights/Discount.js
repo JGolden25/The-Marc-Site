809fac83e2ed49212c37e0ce809f816267b80fa1
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-18 14:26:22 -0600

    Discount and Hightlight transitions and text included

---
+++ src/components/Highlights/Discount.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+
+class Discount extends Component {
+
+state = {
+    discountStart:0,
+    discountEnd: 30
+}
+
+    render() {
+        return (
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                <Fade>
+                <div className="discount_porcentage">
+                    <span>20%</span>
+                    <span>OFF</span>
+                    </div>
+                </Fade>
+
+                <Slide right>
+                <div className="discount_description">
+                    <h3>Purchase tickets before Dec 20th</h3>
+                    <p>lipsumfornow</p>
+                    <div>
+                    button
+                    </div>
+                      
+                    </div>
+                </Slide>
+                    
+                </div>
+                </div>
+        );
+    }
+}
+
+export default Discount;
--- src/components/Highlights/index.js
+++ src/components/Highlights/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Description from './Description';
+import Discount from './Discount';
 const Highlights = () => {
     return (
         <div className="highlight_wrapper">
             <Description/>
+            <Discount/>
         </div>
     );
 };
